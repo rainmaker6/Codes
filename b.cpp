c24b13e0cfbef417355cfef3b85c4fedaa63da0f
rainmaker6 <masteroftheirclassic@gmail.com>
2021-04-14 13:12:04 +0530

    Final Test

--- b.cpp
+++ b.cpp
@@ -1,90 +1,136 @@
-// A C++ program for Dijkstra's single source shortest path algorithm.
-// The program is for adjacency matrix representation of the graph
+// A C++ program for Bellman-Ford's single source
+// shortest path algorithm.
+#include <bits/stdc++.h>
 
-#include <limits.h>
-#include <stdio.h>
+// a structure to represent a weighted edge in graph
+struct Edge {
+	int src, dest, weight;
+};
 
-// Number of vertices in the graph
-#define V 9
+// a structure to represent a connected, directed and
+// weighted graph
+struct Graph {
+	// V-> Number of vertices, E-> Number of edges
+	int V, E;
 
-// A utility function to find the vertex with minimum distance value, from
-// the set of vertices not yet included in shortest path tree
-int minDistance(int dist[], bool sptSet[])
+	// graph is represented as an array of edges.
+	struct Edge* edge;
+};
+
+// Creates a graph with V vertices and E edges
+struct Graph* createGraph(int V, int E)
 {
-	// Initialize min value
-	int min = INT_MAX, min_index;
-
-	for (int v = 0; v < V; v++)
-		if (sptSet[v] == false && dist[v] <= min)
-			min = dist[v], min_index = v;
-
-	return min_index;
+	struct Graph* graph = new Graph;
+	graph->V = V;
+	graph->E = E;
+	graph->edge = new Edge[E];
+	return graph;
 }
 
-// A utility function to print the constructed distance array
-void printSolution(int dist[])
+// A utility function used to print the solution
+void printArr(int dist[], int n)
 {
-	printf("Vertex \t\t Distance from Source\n");
-	for (int i = 0; i < V; i++)
+	printf("Vertex Distance from Source\n");
+	for (int i = 0; i < n; ++i)
 		printf("%d \t\t %d\n", i, dist[i]);
 }
 
-// Function that implements Dijkstra's single source shortest path algorithm
-// for a graph represented using adjacency matrix representation
-void dijkstra(int graph[V][V], int src)
+// The main function that finds shortest distances from src to
+// all other vertices using Bellman-Ford algorithm. The function
+// also detects negative weight cycle
+void BellmanFord(struct Graph* graph, int src)
 {
-	int dist[V]; // The output array. dist[i] will hold the shortest
-	// distance from src to i
+	int V = graph->V;
+	int E = graph->E;
+	int dist[V];
 
-	bool sptSet[V]; // sptSet[i] will be true if vertex i is included in shortest
-	// path tree or shortest distance from src to i is finalized
-
-	// Initialize all distances as INFINITE and stpSet[] as false
+	// Step 1: Initialize distances from src to all other vertices
+	// as INFINITE
 	for (int i = 0; i < V; i++)
-		dist[i] = INT_MAX, sptSet[i] = false;
-
-	// Distance of source vertex from itself is always 0
+		dist[i] = INT_MAX;
 	dist[src] = 0;
 
-	// Find shortest path for all vertices
-	for (int count = 0; count < V - 1; count++) {
-		// Pick the minimum distance vertex from the set of vertices not
-		// yet processed. u is always equal to src in the first iteration.
-		int u = minDistance(dist, sptSet);
-
-		// Mark the picked vertex as processed
-		sptSet[u] = true;
-
-		// Update dist value of the adjacent vertices of the picked vertex.
-		for (int v = 0; v < V; v++)
-
-			// Update dist[v] only if is not in sptSet, there is an edge from
-			// u to v, and total weight of path from src to v through u is
-			// smaller than current value of dist[v]
-			if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX
-				&& dist[u] + graph[u][v] < dist[v])
-				dist[v] = dist[u] + graph[u][v];
+	// Step 2: Relax all edges |V| - 1 times. A simple shortest
+	// path from src to any other vertex can have at-most |V| - 1
+	// edges
+	for (int i = 1; i <= V - 1; i++) {
+		for (int j = 0; j < E; j++) {
+			int u = graph->edge[j].src;
+			int v = graph->edge[j].dest;
+			int weight = graph->edge[j].weight;
+			if (dist[u] != INT_MAX && dist[u] + weight < dist[v])
+				dist[v] = dist[u] + weight;
+		}
 	}
 
-	// print the constructed distance array
-	printSolution(dist);
+	// Step 3: check for negative-weight cycles. The above step
+	// guarantees shortest distances if graph doesn't contain
+	// negative weight cycle. If we get a shorter path, then there
+	// is a cycle.
+	for (int i = 0; i < E; i++) {
+		int u = graph->edge[i].src;
+		int v = graph->edge[i].dest;
+		int weight = graph->edge[i].weight;
+		if (dist[u] != INT_MAX && dist[u] + weight < dist[v]) {
+			printf("Graph contains negative weight cycle");
+			return; // If negative cycle is detected, simply return
+		}
+	}
+
+	printArr(dist, V);
+
+	return;
 }
 
-// driver program to test above function
+// Driver program to test above functions
 int main()
 {
-	/* Let us create the example graph discussed above */
-	int graph[V][V] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-						{ 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-						{ 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-						{ 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-						{ 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-						{ 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-						{ 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-						{ 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-						{ 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+	/* Let us create the graph given in above example */
+	int V = 5; // Number of vertices in graph
+	int E = 8; // Number of edges in graph
+	struct Graph* graph = createGraph(V, E);
 
-	dijkstra(graph, 0);
+	// add edge 0-1 (or A-B in above figure)
+	graph->edge[0].src = 0;
+	graph->edge[0].dest = 1;
+	graph->edge[0].weight = -1;
+
+	// add edge 0-2 (or A-C in above figure)
+	graph->edge[1].src = 0;
+	graph->edge[1].dest = 2;
+	graph->edge[1].weight = 4;
+
+	// add edge 1-2 (or B-C in above figure)
+	graph->edge[2].src = 1;
+	graph->edge[2].dest = 2;
+	graph->edge[2].weight = 3;
+
+	// add edge 1-3 (or B-D in above figure)
+	graph->edge[3].src = 1;
+	graph->edge[3].dest = 3;
+	graph->edge[3].weight = 2;
+
+	// add edge 1-4 (or A-E in above figure)
+	graph->edge[4].src = 1;
+	graph->edge[4].dest = 4;
+	graph->edge[4].weight = 2;
+
+	// add edge 3-2 (or D-C in above figure)
+	graph->edge[5].src = 3;
+	graph->edge[5].dest = 2;
+	graph->edge[5].weight = 5;
+
+	// add edge 3-1 (or D-B in above figure)
+	graph->edge[6].src = 3;
+	graph->edge[6].dest = 1;
+	graph->edge[6].weight = 1;
+
+	// add edge 4-3 (or E-D in above figure)
+	graph->edge[7].src = 4;
+	graph->edge[7].dest = 3;
+	graph->edge[7].weight = -3;
+
+	BellmanFord(graph, 0);
 
 	return 0;
 }
