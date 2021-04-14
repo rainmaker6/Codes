b90384660f660ba0b4fbd476a8106965a07bc96e
rainmaker6 <masteroftheirclassic@gmail.com>
2021-04-14 13:18:11 +0530

    Testing test_run_time

--- c.cpp
+++ c.cpp
@@ -1,72 +1,41 @@
 #include <bits/stdc++.h>
 
+const int MOD = 998244353;
+int minv(int a, int m) {
+	return a == 1 ? 1 : m - int(int64_t(minv(m % a, a)) * m / a);
+}
+
 int main() {
 	using namespace std;
 	ios_base::sync_with_stdio(false), cin.tie(nullptr);
 
-	int N; cin >> N;
-	vector<array<int, 2>> cards(2*N);
-	vector<int> other(4*N);
-	vector<int> card_id(4*N);
-	for (int i = 0; i < 2*N; i++) {
-		array<int, 2> card;
-		for (int z = 0; z < 2; z++) {
-			int a; cin >> a;
-			cards[i][z] = a;
-			card[z] = (z*N + (abs(a)-1)) * 2 + (a < 0);
+	int M; cin >> M;
+	int N = 0;
+	vector<int> A(M); for (auto& a : A) { cin >> a; N += a; }
+
+	auto get_val = [&](int a) {
+		assert(a >= 1);
+		int64_t denom = 1;
+		int64_t numer_cur = 2;
+		int64_t numer_tot = numer_cur;
+		for (int x = 1; x < a; x++) {
+			numer_cur *= 2*N-x;
+			numer_tot *= N-x;
+			denom *= N-x;
+			numer_tot += numer_cur;
+			numer_cur %= MOD;
+			numer_tot %= MOD;
+			denom %= MOD;
 		}
-		card_id[card[0]] = card_id[card[1]] = i;
-		other[card[0]] = card[1];
-		other[card[1]] = card[0];
+		return int(numer_tot * minv(int(denom), MOD) % MOD);
+	};
+
+	int ans = get_val(N);
+	for (int a : A) {
+		ans -= get_val(a);
+		ans = (ans < 0 ? ans + MOD : ans);
 	}
-
-	vector<bool> vis(2*N);
-	vector<int> nxt(2*N, -1);
-	int cur_path = -1;
-
-	vector<pair<array<int, 2>, bool>> stk(2*N);
-
-	for (int st = 0; st < 2*N; st++) {
-		if (vis[st]) continue;
-
-		int lo = 0, hi = 0;
-		for (int e = 2*st+0; !vis[e>>1]; e = other[e^1]) {
-			vis[e>>1] = true;
-			array<int, 2> cur{card_id[e], card_id[e]};
-			if (hi >= 2 && stk[hi-2].second == stk[hi-1].second) {
-				array<int, 2> a = stk[hi-2].first, b = stk[hi-1].first, c = cur;
-				if (stk[hi-1].second) swap(a, c);
-				hi -= 2;
-				nxt[a[1]] = b[0];
-				nxt[b[1]] = c[0];
-				cur = {a[0], c[1]};
-			}
-			stk[hi++] = {cur, e&1};
-		}
-		if (hi >= 2 && stk[hi-2].second == stk[hi-1].second) {
-			array<int, 2> a = stk[hi-2].first, b = stk[hi-1].first, c = stk[lo].first;
-			if (stk[hi-1].second) swap(a, c);
-			hi -= 2;
-			nxt[a[1]] = b[0];
-			nxt[b[1]] = c[0];
-			stk[lo].first = {a[0], c[1]};
-		}
-		if (hi - lo != 2) {
-			cout << "NO" << '\n';
-			exit(0);
-		}
-		if (stk[lo].second) {
-			swap(stk[lo], stk[lo+1]);
-		}
-		nxt[stk[lo+1].first[1]] = cur_path;
-		nxt[stk[lo].first[1]] = stk[lo+1].first[0];
-		cur_path = stk[lo].first[0];
-	}
-
-	cout << "YES" << '\n';
-	for (int i = cur_path; i != -1; i = nxt[i]) {
-		cout << cards[i][0] << ' ' << cards[i][1] << '\n';
-	}
+	cout << ans << '\n';
 
 	return 0;
 }
