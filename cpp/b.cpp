7637d52a5247d56ecb6586cf631d33e2b22f15a6
rainmaker6 <masteroftheirclassic@gmail.com>
2021-05-03 10:47:03 +0530

    ci

--- cpp/b.cpp
+++ cpp/b.cpp
@@ -1,38 +1,8 @@
-#include <iostream>
-#include <vector>
-
+#include<iostream>
 using namespace std;
-
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  int tt;
-  cin >> tt;
-  while (tt--) {
-    int x, y;
-    cin >> x >> y;
-    vector<int> bx;
-    vector<int> by;
-    for (int bit = 0; bit < 30; bit++) {
-      if (x & (1 << bit)) {
-        bx.push_back(bit);
-      }
-      if (y & (1 << bit)) {
-        by.push_back(bit);
-      }
-    }
-    bool ok = true;
-    if (bx.size() < by.size()) {
-      ok = false;
-    } else {
-      for (int i = 0; i < (int) by.size(); i++) {
-        if (bx[i] > by[i]) {
-          ok = false;
-        }
-      }
-    }
-    ok &= (x <= y);
-    cout << (ok ? "YES" : "NO") << '\n';
-  }
-  return 0;
+int main(){
+    int n;
+    cin>>n;
+    cout<<n<<' '<<"Hello\n"; 
+    return 0;
 }
