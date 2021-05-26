279119504fdaf29e82b41afd5ee05b298e668443
rainmaker6 <masteroftheirclassic@gmail.com>
2021-05-26 12:46:55 +0530

    test

--- examples/tensor/simple_expressions.cpp
+++ examples/tensor/simple_expressions.cpp
@@ -1,16 +1,3 @@
-//
-//  Copyright (c) 2018, Cem Bassoy, cem.bassoy@gmail.com
-//  Copyright (c) 2019, Amit Singh, amitsingh19975@gmail.com
-//
-//  Distributed under the Boost Software License, Version 1.0. (See
-//  accompanying file LICENSE_1_0.txt or copy at
-//  http://www.boost.org/LICENSE_1_0.txt)
-//
-//  The authors gratefully acknowledge the support of
-//  Google and Fraunhofer IOSB, Ettlingen, Germany
-//
-
-
 #include <boost/numeric/ublas/tensor.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/vector.hpp>
